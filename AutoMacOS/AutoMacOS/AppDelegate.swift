92b4c38a365cf876d918cff842c71b01a0447ca7
jiale <jiale@thunisoft.com>
2019-04-15 10:41:03 +0800

    init

---
+++ AutoMacOS/AutoMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoMacOS
+//
+//  Created by ens on 2019/4/15.
+//  Copyright © 2019 jialeens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
