8d3b975e0a693ea9e66fd396c1b70123166e5026
jiale <jiale@thunisoft.com>
2019-04-17 22:19:13 +0800

    🍌增加事件监听

--- AutoMacOS/AppDelegate.swift
+++ AutoMacOS/AppDelegate.swift
@@ -12,15 +12,73 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    @IBAction func openPreference(_ sender: Any) {
+        
+    }
+    @IBAction func appClose(_ sender: Any) {
+        NSApplication.shared.terminate(self);
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "🌞";
+        statusItem.menu = statusMenu;
+        self.addNotifications();
     }
+    
+    /// <#Description#> 增加监听器
+    func addNotifications() {
+        NSLog("fileNotifications", NSNull())
+        NSWorkspace.shared.notificationCenter.addObserver(
+            self, selector: #selector(onWakeNote(note:)),
+            name: NSWorkspace.didWakeNotification, object: nil)
 
+        NSWorkspace.shared.notificationCenter.addObserver(
+            self, selector: #selector(onSleepNote(note:)),
+            name: NSWorkspace.willSleepNotification, object: nil)
+    }
+    
+    /// <#Description#> 唤醒时方法
+    ///
+    /// - Parameter note: <#note description#>
+    @objc func onWakeNote(note: NSNotification) {
+        commandLineTask("/bin/sh",["-c","networksetup -setairportpower 'Wi-Fi' on"])
+        commandLineTask("/usr/local/bin/blueutil",["--power","1"])
+    }
+    
+    /// <#Description#> 休眠时方法
+    ///
+    /// - Parameter note: <#note description#>
+    @objc func onSleepNote(note: NSNotification) {
+        commandLineTask("/bin/sh",["-c","networksetup -setairportpower 'Wi-Fi' off"])
+        commandLineTask("/usr/local/bin/blueutil",["--power","0"])
+    }
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        NSLog("app exit!", NSNull())
     }
-
-
+    
+    /// <#Description#> 执行通用命令方法
+    ///
+    /// - Parameters:
+    ///   - launchpath: <#launchpath description#>
+    ///   - command: <#command description#>
+    func commandLineTask(_ launchpath : String,_ command: [String]) {
+        let process = Process()
+        process.launchPath = launchpath
+        let arguments = command
+        print(arguments)
+        process.arguments = arguments
+        
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        let file = pipe.fileHandleForReading
+        process.launch()
+        let data = file.readDataToEndOfFile()
+        let string = String(data: data, encoding: .utf8)
+        
+        print("got \n" + (string ?? ""))
+    }
 }
 
